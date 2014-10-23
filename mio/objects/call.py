fb9549a99f513925bed7ff0d65703db3c9aadffe
James Mills <prologic@shortcircuit.net.au>
2014-10-24 01:19:49 +1000

    Implemented getter methods on Call

--- mio/objects/call.py
+++ mio/objects/call.py
@@ -1,7 +1,12 @@
+from ..registry import Registry
+
+
 from .object import W_Object
 
 
 class W_Call(W_Object):
+
+    registry = Registry()
 
     def __init__(self, space, receiver, context, message, parent=None):
         W_Object.__init__(self, space, parent=(parent or space.object))
@@ -19,3 +24,21 @@ class W_Call(W_Object):
         return W_Call(
             self.space, receiver, context, message, parent=self
         )
+
+    @registry.register("receiver")
+    def m_receiver(self, space, receiver, context, message):
+        """Return receiver"""
+
+        return self.receiver
+
+    @registry.register("context")
+    def m_context(self, space, receiver, context, message):
+        """Return context"""
+
+        return self.context
+
+    @registry.register("message")
+    def m_message(self, space, receiver, context, message):
+        """Return message"""
+
+        return self.message
